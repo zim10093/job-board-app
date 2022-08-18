bbcddf187501ce8e8017cb2a03fc0dc8ba4c45ba
Igor <ZIM20093@gmail.com>
2022-08-18 12:27:40 +0300

    setUp project

---
+++ src/test/java/base/core/faceit/JobBoardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package base.core.faceit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
