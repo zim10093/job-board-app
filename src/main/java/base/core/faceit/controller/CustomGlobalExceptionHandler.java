7e60eea1f79f9e13585b242a65e0cc3dada326be
Ihor Bondar <zim20093@gmail.com>
2022-09-06 23:05:59 +0300

    added GlobalExceptionHandler

---
+++ src/main/java/base/core/faceit/controller/CustomGlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package base.core.faceit.controller;
+
+import base.core.faceit.exception.SyncException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({NoSuchElementException.class, SyncException.class})
+    protected ResponseEntity<Object> handleDataProcessingException(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now().toString());
+        body.put("message", ex.getMessage());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
